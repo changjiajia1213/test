6dd25c96abfb7901f6321e52782aae15ae9a7ec4
changjiajia <2245741634@qq.com>
2020-03-02 15:10:40 +0800

    小二第二次周考

--- src/test/java/com/changjiajia/redis/test/MyTest.java
+++ src/test/java/com/changjiajia/redis/test/MyTest.java
@@ -28,7 +28,9 @@ public class MyTest {
 		for (int i = 1; i <= 100000; i++) {
 			User user = new User();
 			user.setId(i);
+			// 使用随机数工具类中随机获取三个中文作为姓名
 			user.setName(RandomUtil.getRandomChineseString(3));
+			// 随机生成0或1 0是男 1是女
 			Random random = new Random();
 			int sexRdm = random.nextInt(2);
 			if (sexRdm == 0) {
@@ -36,12 +38,15 @@ public class MyTest {
 			} else if (sexRdm == 1) {
 				user.setSex("女");
 			}
+			// 随机数工具类获取9个数字 追加到号码后缀字符串上
 			int[] phones = RandomUtil.getRandomNum(9);
 			String phone = "";
 			for (int j : phones) {
 				phone += j;
 			}
 			user.setPhone("13" + phone);
+			// 生成3-20中任意一位随机数作为邮箱前缀（调用工具类传入生成的随机数）
+			// 生成0-6中任意一位随机数作为邮箱后缀的判断条件
 			int emailRdm = random.nextInt(17) + 3;
 			int emailRdmEnd = random.nextInt(7);
 			if (emailRdmEnd == 0) {
@@ -59,14 +64,17 @@ public class MyTest {
 			} else if (emailRdmEnd == 6) {
 				user.setEmail(RandomUtil.getRandomCNumberString(emailRdm) + "@foxmail.com");
 			}
+			// 获取一位1949-2001中的随机数作为年份 1-12中的随机数作为月份 1-30中的随机数作为日
 			int yearRdm = random.nextInt(52) + 1949;
 			int monthRdm = RandomUtil.getRandomNum(1, 12);
 			int dayRdm = RandomUtil.getRandomNum(1, 30);
 			user.setBirthday(yearRdm + "-" + monthRdm + "-" + dayRdm);
 			// System.out.println(user);
+			// JDK系列化方式和JSON系列化方式的测试
 			// redisTemplate.opsForValue().set("u" + i, user);
 			hashMap.put("u" + i, user);
 		}
+		// hash类型
 		redisTemplate.opsForHash().putAll("myhash", hashMap);
 		long endTime = System.currentTimeMillis();
 		// System.out.println("序列化方式：JDK系列化方式");
